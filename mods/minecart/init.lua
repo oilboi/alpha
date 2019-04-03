00131822e4ac286b3e4fbfb7c15fb00283560064
oilboi <47129783+oilboi@users.noreply.github.com>
2019-04-03 19:05:01 -0400

    Allow minecarts to turn and introduce friction to them

--- mods/minecart/init.lua
+++ mods/minecart/init.lua
@@ -9,25 +9,43 @@ local minecart = {
     visual_size = {x=1, y=1},
     textures = {"minecart_ent.png"},
     --automatic_face_movement_dir = 90.0,
+    timer = 0,
+    speed = 0,
   },
+
 }
 function minecart:on_activate(staticdata, dtime_s)
-    self.timer = 0
-    self.speed = 0
     --in the future make this check for players around and they will "push it"
     self.object:set_acceleration({x=0,y=-10,z=0})
 
     if staticdata ~= "" and staticdata ~= nil then
         local data = minetest.parse_json(staticdata) or {}
+        --restore old data
+        if data then
+          self.timer = data.timer
+          self.speed = data.speed
+          self.old_velocity = data.old_velocity
+        end
     end
 end
 
 function minecart:on_step(dtime)
   --add_velocity(vel)
   minecart:repel(self)
+  minecart:change_direction(self)
+  minecart:friction(self)
+
   local vel = self.object:get_velocity()
 
   self.old_velocity = vel
+end
+
+--slow down cart with friction
+function minecart:friction(self)
+  local vel = self.object:getvelocity()
+  vel = vector.multiply(vel,-1)
+  vel = vector.divide(vel,100)
+  self.object:add_velocity(vel)
 end
 
 --push away from players
@@ -45,9 +63,29 @@ function minecart:repel(self)
     end
   end
 end
+--turn corners on rails
+function minecart:change_direction(self)
+  local vel = self.object:get_velocity()
+  local pos = self.object:getpos()
+  --stopped on the x axis
 
-function minecart:change_direction()
-
+  if self.old_velocity and math.abs(self.old_velocity.x) > math.abs(self.old_velocity.z) and vel.x == 0 then
+    print("boing")
+    --make it turn
+    if minetest.get_node({x=pos.x,y=pos.y,z=pos.z-1}).name == ("air" or "nodes:rail_straight") then
+      self.object:set_velocity({x=0,y=self.old_velocity.y,z=math.abs(self.old_velocity.x)*-1})
+    elseif minetest.get_node({x=pos.x,y=pos.y,z=pos.z+1}).name == ("air" or "nodes:rail_straight") then
+      self.object:set_velocity({x=0,y=self.old_velocity.y,z=math.abs(self.old_velocity.x)})
+    end
+  elseif self.old_velocity and math.abs(self.old_velocity.z) > math.abs(self.old_velocity.x) and vel.z == 0 then
+    print(self.old_velocity.z)
+    --make it turn
+    if minetest.get_node({x=pos.x-1,y=pos.y,z=pos.z}).name == ("air" or "nodes:rail_straight") then
+      self.object:set_velocity({x=math.abs(self.old_velocity.z)*-1,y=self.old_velocity.y,z=0})
+    elseif minetest.get_node({x=pos.x+1,y=pos.y,z=pos.z}).name == ("air" or "nodes:rail_straight") then
+      self.object:set_velocity({x=math.abs(self.old_velocity.z),y=self.old_velocity.y,z=0})
+    end
+  end
 end
 
 
@@ -58,6 +96,9 @@ end
 function minecart:get_staticdata()
     return minetest.write_json({
         message = self.message,
+        timer   = self.timer,
+        speed   = self.speed,
+        old_velocity = self.old_velocity,
     })
 end
 
