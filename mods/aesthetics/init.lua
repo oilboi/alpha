f01208f4a858a9d0825c35a2a6f55af377a7218d
oilboi <oil.boi.minetest@gmail.com>
2019-04-07 13:00:42 -0400

    fix particle explosion and make tools make noise with particles when broken

--- mods/aesthetics/init.lua
+++ mods/aesthetics/init.lua
@@ -4,17 +4,17 @@
    local node = oldnode.name
    local tile = minetest.registered_nodes[node].tiles
 
-   mining_explosion(tile,pos,25,90,140,1,2)
+   mining_particle_explosion(tile,pos,25,90,140,10,20)
 end)
 
 --tile is the tile table,pos,amount minimum, math.random(amount_max,amount_max2)
-function mining_explosion(tile,pos,amount_min,amount_max,amount_max2,time_min,time_max)
+function mining_particle_explosion(tile,pos,amount_min,amount_max,amount_max2,time_min,time_max)
   for i = amount_min,math.random(amount_max,amount_max2) do
     --select random part of the texture
     local texsizer = math.random(1,3)
     local size = texsizer/3 --the texture size affects the particle size for consistancy
     local texsize = {x=texsizer,y=texsizer}
-    local texpos = {x=math.random(1,16-texsize.x)/64,y=math.random(1,16-texsize.y)/64}
+    local texpos = {x=math.random(-16,-1-texsizer),y=math.random(-16,-1-texsizer)}
     --filename1
     local texture = "[combine:"..texsize.x.."x"..texsize.y..":"..texpos.x..","..texpos.y.."="..tile[math.random(1,table.getn(tile))]
     minetest.add_particle({
@@ -33,4 +33,25 @@ function mining_explosion(tile,pos,amount_min,amount_max,amount_max2,time_min,ti
       texture = texture,
     })
   end
+end
+
+
+--turn tools into stick when dead and break sound with particles
+function tool_break(itemstack, user, node, digparams)
+  local oldstack = itemstack:get_name()
+  itemstack:add_wear(digparams.wear)
+  if itemstack:get_wear() == 0 and digparams.wear > 0 then
+    minetest.sound_play("tool_break", {
+      pos = user:getpos(),
+      max_hear_distance = 100,
+      gain = 1.0,
+    })
+
+    --itemstack:replace("items:stick")
+    local pos1 = user:getpos()
+    pos1.y = pos1.y + 1.5
+    local tile = {minetest.registered_items[oldstack].wield_image}
+    mining_particle_explosion(tile,pos1,25,90,140,1,2)
+  end
+  return(itemstack)
 end
