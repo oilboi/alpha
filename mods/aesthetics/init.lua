66ac79b4f6e9fb13186ddcab8bb9545c22372e11
oilboi <oil.boi.minetest@gmail.com>
2019-04-07 11:41:43 -0400

    better particles

--- mods/aesthetics/init.lua
+++ mods/aesthetics/init.lua
@@ -5,27 +5,33 @@
    local node = oldnode.name
    local tile = minetest.registered_nodes[node].tiles
 
-   --select random part of the texture
-   local texsizer = math.random(1,3)
-   local size = texsizer/3 --the texture size affects the particle size for consistancy
-   local texsize = {x=texsizer,y=texsizer}
-   local texpos = {x=math.random(1,16-texsize.x)/64,y=math.random(1,16-texsize.y)/64}
-   --filename1
-   local texture = "[combine:"..texsize.x.."x"..texsize.y..":"..texpos.x..","..texpos.y.."="..tile[math.random(1,table.getn(tile))]
-
-   for i = 25,math.random(50,70) do
-   minetest.add_particle({
-     pos = pos,
-     velocity = {x=math.random(-2,2)+(math.random()*math.random(-1,1)), y=math.random(2,4)+(math.random()*math.random(-1,1)), z=math.random(-2,2)+(math.random()*math.random(-1,1))},
-     acceleration = {x=0, y=-10, z=0},
-     expirationtime = math.random(1,2),
-     size = size,
-     collisiondetection = true,
-     collision_removal = false,
-     object_collision = true,
-     vertical = false,
-     texture = texture,
-
-   })
- end
+   particle_explosion(tile,pos,25,90,140,1,2)
 end)
+
+--tile is the tile table,pos,amount minimum, math.random(amount_max,amount_max2)
+function particle_explosion(tile,pos,amount_min,amount_max,amount_max2,time_min,time_max)
+  for i = amount_min,math.random(amount_max,amount_max2) do
+    --select random part of the texture
+    local texsizer = math.random(1,3)
+    local size = texsizer/3 --the texture size affects the particle size for consistancy
+    local texsize = {x=texsizer,y=texsizer}
+    local texpos = {x=math.random(1,16-texsize.x)/64,y=math.random(1,16-texsize.y)/64}
+    --filename1
+    local texture = "[combine:"..texsize.x.."x"..texsize.y..":"..texpos.x..","..texpos.y.."="..tile[math.random(1,table.getn(tile))]
+    minetest.add_particle({
+      pos = pos,
+      velocity = {
+      x=math.random()*math.random(-3,3),
+      y=math.random(2,4),
+      z=math.random()*math.random(-3,3)},
+      acceleration = {x=0, y=-10, z=0},
+      expirationtime = math.random(time_min,time_max)/2,
+      size = size,
+      collisiondetection = true,
+      collision_removal = false,
+      object_collision = true,
+      vertical = false,
+      texture = texture,
+    })
+  end
+end
