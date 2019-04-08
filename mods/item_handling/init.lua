026d447d263b7c0f5e46bd98ecada094f40b6224
oilboi <47129783+oilboi@users.noreply.github.com>
2019-04-08 15:31:29 -0400

    change default throw to single item and sneak throw to stack

--- mods/item_handling/init.lua
+++ mods/item_handling/init.lua
@@ -6,6 +6,9 @@ minetest.register_on_mods_loaded(function()
 	for node,def in pairs(minetest.registered_nodes) do
 		minetest.override_item(node, {stack_max = 64})
 	end
+	for node,def in pairs(minetest.registered_tools) do
+		minetest.override_item(node, {stack_max = 1})
+	end
 end)
 
 --this is built off of Wuzzy's Mineclone2 item magnet
@@ -73,11 +76,59 @@ function minetest.handle_node_drops(pos, drops, digger)
 		drop_item:set_count(1)
 
 		for i=1,count do
-			local obj = core.add_item(pos, drop_item)
+			local obj = minetest.add_item(pos, drop_item)
 			if obj ~= nil then
         obj:get_luaentity().age = collection_age - 0.35 --make sure collected on dig - 0.5 for aesthetics
 				obj:set_velocity({x=math.random(-2,2)*math.random(), y=obj:get_velocity().y, z=math.random(-2,2)*math.random()})
 			end
+		end
+	end
+end
+
+--throw a single item at player's speed
+--sneak throw, throws whole stack
+function minetest.item_drop(itemstack, dropper, pos)
+
+	local dropper_is_player = dropper and dropper:is_player()
+
+	local p = table.copy(pos)
+
+	local vel = dropper:get_player_velocity()
+
+	if dropper_is_player then
+		p.y = p.y + 1.5
+	end
+
+	local sneak = dropper:get_player_control().sneak
+	--sneaking throws whole stack
+
+	--do this to override the builting shift drop
+	local inv = dropper:get_inventory()
+	local index = dropper:get_wield_index()
+	local wield_list = dropper:get_wield_list()
+
+
+	local count = 1
+	if sneak == true then
+		count = inv:get_stack(wield_list, index):get_count()
+	end
+
+	--brute force past default sneak throw
+	local itemstack2 = inv:get_stack(wield_list, index)
+	local old_item = itemstack2:take_item(count)
+	inv:set_stack(wield_list, index, itemstack2)
+
+	local obj = minetest.add_item(p, old_item)
+
+	if obj then
+		if dropper_is_player then
+			local dir = dropper:get_look_dir()
+			dir.x = vel.x + (dir.x * 2.9)
+			dir.y = vel.y + (dir.y * 2.9 + 2)
+			dir.z = vel.z + (dir.z * 2.9)
+			obj:set_velocity(dir)
+			obj:get_luaentity().dropped_by = dropper:get_player_name()
+			obj:get_luaentity().timer = 4 --debug
 		end
 	end
 end
