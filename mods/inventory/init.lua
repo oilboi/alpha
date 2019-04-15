f19092154f22e65da1a606f3d3474f51134f7c37
oilboi <47129783+oilboi@users.noreply.github.com>
2019-04-14 21:05:15 -0400

    Add mineclone2 inventory textures

--- mods/inventory/init.lua
+++ mods/inventory/init.lua
@@ -8,10 +8,12 @@ minetest.register_on_joinplayer(function(player)
 
 
 	--set hotbar size
-	player:hud_set_hotbar_itemcount(8)
+	player:hud_set_hotbar_itemcount(9)
 
   --gui formspec
   local form = "size[9,8.75]"..
+  --texture
+  "background[-0.19,-0.25;9.41,9.49;crafting_formspec_bg.png]"..
   -- main gui
   "list[current_player;main;0,4.5;9,3;9]"..
   "list[current_player;main;0,7.74;9,1;]"..
@@ -21,5 +23,10 @@ minetest.register_on_joinplayer(function(player)
 
   --set the formspec
   player:set_inventory_formspec(form)
+
+
+  --add hotbar images
+	player:hud_set_hotbar_image("gui_hotbar.png")
+	player:hud_set_hotbar_selected_image("gui_hotbar_selected.png")
 
 end)
