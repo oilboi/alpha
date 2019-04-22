8de71a4157bfff133de1041aa6928dc12750dded
oilboi <47129783+oilboi@users.noreply.github.com>
2019-04-22 04:12:06 -0400

    reduce fall bobbing

--- mods/zzzzzzzzzzzzz_helpers/init.lua
+++ mods/zzzzzzzzzzzzz_helpers/init.lua
@@ -16,4 +16,4 @@ minetest.settings:set("liquid_update", 0.3)
 
 --set the bobbing
 minetest.settings:set("view_bobbing_amount", 2.0)
-minetest.settings:set("fall_bobbing_amount", 2.0)
+minetest.settings:set("fall_bobbing_amount", 0)
